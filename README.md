cd59e3b5128cb5e6681a3bafbbd2d4b310d5fea2
ABC <85392015@qq.com>
2022-06-07 13:42:57 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# commchain-website
+# Hello Godel Website
+
---
+++ build/precache-manifest.2aea8263be3ce2fb67f34ccde084a891.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44e769854b12921eb54f",
+    "url": "./static/css/main.1fd76939.chunk.css"
+  },
+  {
+    "revision": "44e769854b12921eb54f",
+    "url": "./static/js/main.44e76985.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "cfe2cd21078798e0e48a",
+    "url": "./static/css/2.2dd81567.chunk.css"
+  },
+  {
+    "revision": "cfe2cd21078798e0e48a",
+    "url": "./static/js/2.cfe2cd21.chunk.js"
+  },
+  {
+    "revision": "959b6d66b6d7afb8bac2cd301eeedb64",
+    "url": "./index.html"
+  }
+];
